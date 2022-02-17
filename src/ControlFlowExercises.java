2c3b5211075dc9577051485b9531d98dad21d685
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-17 13:41:59 -0700

    finished grades converter and bonus section

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -52,16 +52,65 @@ public class ControlFlowExercises {
 //        }
 
         // TABLE OF POWERS
-//        Scanner in = new Scanner(System.in);
-//        System.out.print("What number would you like to go up to? ");
-//        int userInputNum = in.nextInt();
-//        System.out.println("Here is your table!");
-//        System.out.println("number | squared | cubed");
-//        System.out.println("------ | ------- | -----");
-//        for(int i = 1; i <= userInputNum; i++){
-//            int square = (int) Math.pow(i, 2);
-//            int cube = (int) Math.pow(i, 3);
-//            System.out.printf("%-6s | %-7s | %-6s\n", i, square, cube);
+//        boolean displayTable = true;
+//        while(displayTable){
+//            Scanner in = new Scanner(System.in);
+//            System.out.print("What number would you like to go up to? ");
+//            int userInputNum = in.nextInt();
+//            System.out.println("Here is your table!");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for(int i = 1; i <= userInputNum; i++){
+//                int square = (int) Math.pow(i, 2);
+//                int cube = (int) Math.pow(i, 3);
+//                System.out.printf("%-6s | %-7s | %-6s\n", i, square, cube);
+//            }
+//            System.out.print("Would you like to continue? Y/N: ");
+//            String userInputStr = in.next();
+//            if(userInputStr.equalsIgnoreCase("n")){
+//                displayTable = false;
+//                System.out.println("Exiting...");
+//            }
 //        }
+
+        // GRADES CONVERSION
+        Scanner in = new Scanner(System.in);
+        boolean enterGrades = true;
+        while(enterGrades){
+            System.out.print("Enter numerical grade: ");
+            int numberGrade = in.nextInt();
+            if(numberGrade >= 97 && numberGrade <= 100){
+                System.out.println("A+");
+            } else if (numberGrade >= 93 && numberGrade <= 96){
+                System.out.println("A");
+            } else if (numberGrade >= 90 && numberGrade <= 92){
+                System.out.println("A-");
+            } else if (numberGrade >= 87 && numberGrade <= 89){
+                System.out.println("B+");
+            } else if (numberGrade >= 83 && numberGrade <= 86){
+                System.out.println("B");
+            } else if (numberGrade >= 80 && numberGrade <= 82){
+                System.out.println("B-");
+            } else if (numberGrade >= 77 && numberGrade <= 79){
+                System.out.println("C+");
+            } else if (numberGrade >= 73 && numberGrade <= 76){
+                System.out.println("C");
+            } else if (numberGrade >= 70 && numberGrade <= 72){
+                System.out.println("C-");
+            } else if (numberGrade >= 67 && numberGrade <= 69){
+                System.out.println("D+");
+            } else if (numberGrade >= 65 && numberGrade <= 66){
+                System.out.println("D");
+            } else if(numberGrade < 65){
+                System.out.println("F");
+            }
+
+            System.out.print("Would you like to enter another grade? Y/N: ");
+            String userInputStr = in.next();
+            if(userInputStr.equalsIgnoreCase("n")){
+                enterGrades = false;
+                System.out.println("Exiting...");
+            }
+        }
    }
 }
