544209ea0e6081ee9943adaa0695a68cbf8d9110
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-17 13:20:50 -0700

    finished fizzbuzz and table of powers

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         // 1. LOOP BASICS
@@ -34,6 +36,32 @@ public class ControlFlowExercises {
 //
 //        for(long y = 2; y <= 1000000; y *= y){
 //            System.out.println(y);
+//        }
+
+//        // FIZZ BUZZ
+//        for(int i = 1; i <= 100; i++){
+//            if(i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+        // TABLE OF POWERS
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("What number would you like to go up to? ");
+//        int userInputNum = in.nextInt();
+//        System.out.println("Here is your table!");
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        for(int i = 1; i <= userInputNum; i++){
+//            int square = (int) Math.pow(i, 2);
+//            int cube = (int) Math.pow(i, 3);
+//            System.out.printf("%-6s | %-7s | %-6s\n", i, square, cube);
 //        }
    }
 }
