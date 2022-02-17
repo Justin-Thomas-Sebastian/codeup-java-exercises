0c08e7184544f187ba793617b421b9f4b3d016c3
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-17 12:46:50 -0700

    finished loop exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,39 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // 1. LOOP BASICS
+        // a. While
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+//
+        // b. Do While
+//        int j = 0;
+//        do{
+//            System.out.println(j);
+//            j += 2;
+//        }while(j <= 100);
+
+//        int j = 100;
+//        do{
+//            System.out.println(j);
+//            j -= 5;
+//        }while(j >= -5);
+
+//        long j = 2;
+//        do{
+//            System.out.println(j);
+//            j *= j;
+//        }while(j <= 1000000);
+
+//        // C. For Loop
+//        for(int x = 5; x <= 15; x++ ){
+//            System.out.print(x + " ");
+//        }
+//
+//        for(long y = 2; y <= 1000000; y *= y){
+//            System.out.println(y);
+//        }
+   }
+}
