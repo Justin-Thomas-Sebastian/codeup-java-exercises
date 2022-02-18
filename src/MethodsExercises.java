87ca4fe8970cce25681dd2808a381dfcd93cbd2b
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-18 14:37:26 -0700

    finished iterative version of factorial

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,14 +1,22 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println(add(3,3));
-        System.out.println(subtract(10, 2));
-        System.out.println(multiply(5,3));
-        System.out.println(divide(20,4));
-        System.out.println(remainder(50, 7));
-        int userInput = getInteger(1, 10);
-        System.out.println("Validated: " + userInput);
+
+        // Arithmetic methods tests
+//        System.out.println(add(3,3));
+//        System.out.println(subtract(10, 2));
+//        System.out.println(multiply(5,3));
+//        System.out.println(divide(20,4));
+//        System.out.println(remainder(50, 7));
+//
+//        // getInteger test
+//        int userInput = getInteger(1, 10);
+//        System.out.println("Validated: " + userInput);
+
+        // factorial test
+        System.out.println("Factorial: " + getFactorial());
     }
 
     public static int add(int a, int b){
@@ -33,12 +41,20 @@ public class MethodsExercises {
 
     public static int getInteger(int min, int max) {
         Scanner in = new Scanner(System.in);
-        int userInput;
         System.out.print("Enter a number between " + min + " and " + max + ": ");
-        userInput = in.nextInt();
+        int userInput = in.nextInt();
         if (!(userInput >= min && userInput <= max)) {
             userInput = getInteger(min, max);
         }
         return userInput;
+    }
+
+    public static long getFactorial(){
+        int userInput = getInteger(1,10);
+        long factorial = 1;
+        for(int i = 1; i <= userInput; i++){
+            factorial = factorial * i;
+        }
+        return factorial;
     }
 }
