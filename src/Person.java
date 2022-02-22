22b8260d20f9918f7a7ed9ff0d3d299eee4bd3d2
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-22 13:54:16 -0700

    finished object basics exercise

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.println("Name: " + this.name);
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person newPerson = new Person("Justin");
+        System.out.println(newPerson.getName());
+        newPerson.setName("Sebastian");
+        newPerson.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+}
