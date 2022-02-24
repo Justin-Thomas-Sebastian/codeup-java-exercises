d4119d27af5a6253c553d2a146a44fbe2dab7c5e
Justin Sebastian <justinthomassebastian@gmail.com>
2022-02-24 14:53:39 -0700

    finished abstract class and interfaces exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,47 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+/** INHERITANCE/POLYMORPHISM EXERCISE **/
+//public class Rectangle {
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea(){
+//        System.out.println("This method is being called from Rectangle");
+//        return this.length * this.width;
+//    }
+//
+//    public double getPerimeter(){
+//        System.out.println("This method is being called from Rectangle");
+//        return (2 * this.length) + (2 * this.width);
+//    }
+//}
 
+/** INTERFACES/ABSTRACT EXERCISE **/
+public class Rectangle extends Quadrilateral implements Measurable {
     public Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
+        super(length, width);
     }
 
-    public double getArea(){
+    public double getPerimeter() {
+        System.out.println("This method is being called from Rectangle");
+        return (2 * this.length) + (2 * this.width);
+    }
+
+    public double getArea() {
         System.out.println("This method is being called from Rectangle");
         return this.length * this.width;
     }
 
-    public double getPerimeter(){
-        System.out.println("This method is being called from Rectangle");
-        return (2 * this.length) + (2 * this.width);
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
     }
 }
