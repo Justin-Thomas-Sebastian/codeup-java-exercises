29e6bdd614ce3747f41a660152633815824a6336
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-02 10:17:06 -0700

    comment out inheritance/polymorphism exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,45 +1,45 @@
 package shapes;
 
 /** INHERITANCE/POLYMORPHISM EXERCISE **/
-public class Square extends Rectangle{
-    public Square(double side){
-        super(side, side);
-    }
-
-    public double getPerimeter(){
-        System.out.println("This method is being called from Square");
-        return 4 * this.length;
-    }
-
-    public double getArea(){
-        System.out.println("This method is being called from Square");
-        return this.length * this.length;
-    }
-}
-
-/** INTERFACES/ABSTRACT EXERCISE **/
-//public class Square extends Quadrilateral implements Measurable{
+//public class Square extends Rectangle{
 //    public Square(double side){
 //        super(side, side);
 //    }
 //
-//    public double getPerimeter() {
+//    public double getPerimeter(){
 //        System.out.println("This method is being called from Square");
 //        return 4 * this.length;
 //    }
 //
-//    public double getArea() {
+//    public double getArea(){
 //        System.out.println("This method is being called from Square");
 //        return this.length * this.length;
 //    }
-//
-//    public void setLength(double length) {
-//        this.length = length;
-//        this.width = length;
-//    }
-//
-//    public void setWidth(double width) {
-//        this.length = width;
-//        this.width = width;
-//    }
 //}
+
+/** INTERFACES/ABSTRACT EXERCISE **/
+public class Square extends Quadrilateral implements Measurable{
+    public Square(double side){
+        super(side, side);
+    }
+
+    public double getPerimeter() {
+        System.out.println("This method is being called from Square");
+        return 4 * this.length;
+    }
+
+    public double getArea() {
+        System.out.println("This method is being called from Square");
+        return this.length * this.length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    public void setWidth(double width) {
+        this.length = width;
+        this.width = width;
+    }
+}
